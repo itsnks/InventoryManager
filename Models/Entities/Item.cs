764459a47e42bc2ae36bb95b5fae1d79814e7c92
Nikesh <subedi.nikesh@outlook.com>
2025-06-22 09:21:08 +0545

    added item entity

---
+++ Models/Entities/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManager.Models.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public float ItemQuantity { get; set; }
+        public float ItemRate { get; set; }
+    }
+}
