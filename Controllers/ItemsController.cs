37b3e9bc115151885b8596035723f11b9a0836fe
Nikesh <subedi.nikesh@outlook.com>
2025-06-22 09:32:51 +0545

    fuction to get all items

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using InventoryManager.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _db;
+        public ItemsController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var allItems = _db.Items.ToString();
+            return Ok();
+        }    
+    }
+}
