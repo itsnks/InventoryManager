bf9e3b4291c9e28287053200f6a3aeac52ef2020
Nikesh <subedi.nikesh@outlook.com>
2025-06-22 09:21:52 +0545

    dbcontext for items

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManager.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManager.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+        }
+        DbSet<Item> Items { get; set; }
+    }
+}
