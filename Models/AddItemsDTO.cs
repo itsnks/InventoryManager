27631636ac9aa0790c0262a6960f9b8dfa87a4f0
Nikesh <subedi.nikesh@outlook.com>
2025-06-23 09:20:15 +0545

    DTO for AddItems

---
+++ Models/AddItemsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManager.Models
+{
+    public class AddItemsDTO
+    {
+        public int Id { get; set; }
+        public required string ItemName { get; set; }
+        public required float ItemQuantity { get; set; }
+        public required float ItemRate { get; set; }
+    }
+}
