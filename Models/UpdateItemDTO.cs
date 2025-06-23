490be981025cc110225e1724e7b8ab946ccddecd
Nikesh <subedi.nikesh@outlook.com>
2025-06-23 09:38:31 +0545

    dto for updating items

---
+++ Models/UpdateItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManager.Models
+{
+    public class UpdateItemDTO
+    {
+        public int Id { get; set; }
+        public required string ItemName { get; set; }
+        public required float ItemQuantity { get; set; }
+        public required float ItemRate { get; set; }
+    }
+}
